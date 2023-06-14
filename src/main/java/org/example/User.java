baf2efcfe01641db910928fe88f851abc6119513
Valstion <lordvalstion@gmail.com>
2023-06-14 14:53:16 +0300

    Commit

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class User {
+/*
+ * Copyright (c) 2023. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package org.example;
+
+public class User {
+    private String name;
+    private String surName;
+    private String email;
+    private String phoNo;
+    private String passWord;
+
+public User (){
+
+}
+    public User(String name, String surName, String email, String phoNo, String passWord) {
+        this.name = name;
+        this.surName = surName;
+        this.email = email;
+        this.phoNo = phoNo;
+        this.passWord = passWord;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoNo(String phoNo) {
+        this.phoNo = phoNo;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoNo() {
+        return phoNo;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
 }
--- src/test/java/RegisterTests.java
+++ src/test/java/RegisterTests.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class registerTests {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class RegisterTests {
+    static WebDriver driver;
+    @Test
+    public void test() {
+        int min= 1000000;
+        int max = 9999999;
+        int rndNum = min + (int) Math.round(Math.random() * (max - min));
+        driver.findElement(By.id("sylius_customer_registration_firstName")).sendKeys("DEDobosrka");
+        driver.findElement(By.id("sylius_customer_registration_lastName")).sendKeys("SrakaDraka");
+        driver.findElement(By.id("sylius_customer_registration_email")).sendKeys("EFGama23axer" + rndNum + "@gmail.com");
+        driver.findElement(By.xpath("//*[@id=\"website-content\"]/div/div/div/section/form/div/div[5]/div/div/div/div/div/input")).sendKeys("6" +rndNum);
+        driver.findElement(By.id("sylius_customer_registration_user_plainPassword")).sendKeys("Emaxerewq69");
+        driver.findElement(By.xpath("//*[@id=\"website-content\"]/div/div/div/section/form/div/div[7]/div/label/span")).click();
+        driver.findElement(By.xpath("//*[@id=\"website-content\"]/div/div/div/section/form/div/div[8]/input")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.findElement(By.xpath("//*[@id=\"verification-form\"]/button")).click();
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        acceptCookies();
+    }
+
+    public void acceptCookies() {
+        driver.get("https://www.livinn.lt/register");
+        driver.findElement(By.id("onetrust-accept-btn-handler")).click();
+    }
+
+    @AfterClass
+    public void afterClass() {
+       // driver.close();
+    }
 }
+
