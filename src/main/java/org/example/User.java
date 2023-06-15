7227b6da5fbf1217083f2fb7788200de6a7c1721
Valstion <lordvalstion@gmail.com>
2023-06-15 13:57:10 +0300

    Commitdf

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,68 +1,78 @@
-/*
- * Copyright (c) 2023. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- */
-
 package org.example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class User {
+    public static WebDriver driver;
     private String name;
-    private String surName;
+    private String surname;
     private String email;
     private String phoNo;
-    private String passWord;
+    private String password;
 
-public User (){
-
-}
-    public User(String name, String surName, String email, String phoNo, String passWord) {
+    public User() {
+    }
+    public User(String name, String surname, String email, String phoNo, String password) {
         this.name = name;
-        this.surName = surName;
+        this.surname = surname;
         this.email = email;
         this.phoNo = phoNo;
-        this.passWord = passWord;
+        this.password = password;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSurName(String surName) {
-        this.surName = surName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPhoNo(String phoNo) {
-        this.phoNo = phoNo;
-    }
-
-    public void setPassWord(String passWord) {
-        this.passWord = passWord;
+    public void register(){
+        driver.get("https://www.livinn.lt/register");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("sylius_customer_registration_firstName"))).sendKeys(this.name);
+//        driver.findElement(By.id("sylius_customer_registration_firstName")).sendKeys(this.name);
+        driver.findElement(By.id("sylius_customer_registration_lastName")).sendKeys(this.surname);
+        driver.findElement(By.id("sylius_customer_registration_email")).sendKeys(this.email);
+        driver.findElement(By.name("telephone")).sendKeys(this.phoNo);
+        driver.findElement(By.id("sylius_customer_registration_user_plainPassword")).sendKeys(this.password);
+        driver.findElement(By.xpath("//*[@id=\"website-content\"]/div/div/div/section/form/div/div[7]/div/label/span")).click();
+        driver.findElement(By.xpath("//*[@id=\"website-content\"]/div/div/div/section/form/div/div[8]/input")).click();
     }
 
     public String getName() {
         return name;
     }
 
-    public String getSurName() {
-        return surName;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPhoNo() {
         return phoNo;
     }
 
-    public String getPassWord() {
-        return passWord;
+    public void setPhoNo(String phoNo) {
+        this.phoNo = phoNo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
--- src/test/java/RegisterTests.java
+++ src/test/java/RegisterTests.java
@@ -1,6 +1,8 @@
+import org.example.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -76,22 +78,98 @@ public class RegisterTests {
       //  driver.findElement(By.xpath("//*[@id=\"website-content\"]/div/div/div[1]/div/form/div/button")).click();
     }
 
+    @Test
+    public void positiveTest(){
+        User u = new User("jonas","kisielius","jonokisielius10@gmail.com","68479974","Aa3456789");
+        u.register();
+        String text = "";
+        try {
+            text = User.driver.findElement(By.className("form-title")).getText();
+        }catch (Exception e){}
+        Assert.assertEquals(text,"Patvirtinkite paskyrą");
+    }
+
+    @Test
+    public void noEmailAddressTest(){
+        User u = new User("jonas","kisielius","","63777954","Aa3456789");
+        u.register();
+        Assert.assertEquals(errorMsg("email"),"Šis adresas yra neteisingas.");
+    }
+
+    @Test
+    public void emailUserNoEtaNoDomainNoTopDomainTest(){
+        User u = new User("jonas","kisielius","labas","63777954","Aa3456789");
+        u.register();
+        Assert.assertEquals(errorMsg("email"),"Šis adresas yra neteisingas.");
+    }
+
+    @Test
+    public void emailUserEtaDomainNoTopDomainTest(){
+        User u = new User("jonas","kisielius","labas@varle","63777954","Aa3456789");
+        u.register();
+        Assert.assertEquals(errorMsg("email"),"Šis adresas yra neteisingas.");
+    }
+
+    @Test
+    public void emailWithSpaceInUserTest(){
+        User u = new User("jonas","kisielius","labas vakaras@varle.lt","63777954","Aa3456789");
+        u.register();
+        Assert.assertEquals(errorMsg("email"),"Šis adresas yra neteisingas.");
+    }
+
+    @Test
+    public void emailTooLongTest(){
+        User u = new User("jonas","kisielius","labavakara".repeat(60) + "@varle.lt ","63777954","Aa3456789");
+        u.register();
+        Assert.assertEquals(errorMsg("email"),"El. paštas turi būti ilgesnis nei 255 simbolių.");
+    }
+
+    @Test
+    public void emailWithLeadingTreilingSpacesTest(){
+        User u = new User("jonas","kisielius"," labavakara@varle.lt ","63777954","Aa3456789");
+        u.register();
+        Assert.assertEquals(errorMsg("email"),"Šis adresas yra neteisingas.");
+    }
+
+
     @BeforeClass
-    public void beforeClass() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    public void beforeClass(){
+        User.driver = new ChromeDriver();
+        User.driver.manage().window().maximize();
+        User.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         acceptCookies();
     }
 
-    public void acceptCookies() {
-        driver.get("https://www.livinn.lt/register");
-        driver.findElement(By.id("onetrust-accept-btn-handler")).click();
+    public String errorMsg(String field){
+        String errorMsg = "";
+        int position = 0;
+
+        switch (field){
+            case "name":
+                position = 0;
+                break;
+            case "surname":
+                position = 1;
+                break;
+            case "email":
+                position = 2;
+                break;
+        }
+        try {
+            errorMsg = User.driver.
+                    findElement(By.className("login-page-body--form")).
+                    findElements(By.className("form-block")).get(position).
+                    findElement(By.className("field-error")).getText();
+        }catch (Exception e){}
+        return errorMsg;
+    }
+    public void acceptCookies(){
+        User.driver.get("https://www.livinn.lt/register");
+        User.driver.findElement(By.id("onetrust-button-group")).click();
     }
 
     @AfterClass
-    public void afterClass() {
-        driver.close();
+    public void afterClass(){
+        //  driver.close();
     }
 }
-
